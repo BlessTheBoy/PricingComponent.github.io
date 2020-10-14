7deaeb76fe3286bcc9b606858e5ff3da651564fe
Bakare Faruq <bakarefaruq.a@gmail.com>
2020-10-14 03:55:00 -0700

    desktop site completed

---
+++ dist/js/main.js
@@ -0,0 +1,38 @@
+const toggleSwitch = document.getElementById('switch');
+const basicCard = document.getElementById('basic');
+const professinalCard = document.getElementById('professional');
+const masterCard = document.getElementById('master');
+
+let basicMonthly = "19.99";
+let basicAnnually = "199.99";
+
+let professionalMonthly = "24.99";
+let professionalAnnually = "249.99";
+
+let masterMonthly = "39.99";
+let masterAnnually = "399.99";
+
+toggleSwitch.addEventListener('change', (e) => {
+    if (toggleSwitch.checked) {
+        console.log('swith is checked');
+        transformMonthly(basicCard, basicMonthly);
+        transformMonthly(professinalCard, professionalMonthly);
+        transformMonthly(masterCard, masterMonthly);
+    } else {
+        console.log('swith is unchecked');
+        transformAnnually(basicCard, basicAnnually);
+        transformAnnually(professinalCard, professionalAnnually);
+        transformAnnually(masterCard, masterAnnually);
+    }
+});
+
+function transformMonthly (card, price){
+    card.className = 'card monthly';    
+    setTimeout(function(){card.querySelector('.card__price').innerText = price;}, 1500);
+    
+}
+
+function transformAnnually (card, price){
+    card.className = 'card annually';
+    setTimeout(function(){card.querySelector('.card__price').innerText = price;}, 1500);
+}
